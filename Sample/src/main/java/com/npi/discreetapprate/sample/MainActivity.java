cb651293bd20066238bf3d0c69f494d3abd2d37c
Denis Chaschin <denis@chaschin.pro>
2019-04-28 21:09:42 +1000

    Deprecated activity removed

--- Sample/src/main/java/com/npi/discreetapprate/sample/MainActivity.java
+++ Sample/src/main/java/com/npi/discreetapprate/sample/MainActivity.java
@@ -6,7 +6,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,7 +37,7 @@ import fr.nicolaspomepuy.discreetapprate.AppRate;
 import fr.nicolaspomepuy.discreetapprate.AppRateTheme;
 import fr.nicolaspomepuy.discreetapprate.RetryPolicy;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private LinearLayout buttonBar;
     private SharedPreferences settings;
